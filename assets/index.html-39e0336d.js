65196484eb4f6bd9769786278500b931e553e155
64986 <64986@yangzijiang.com>
2023-06-26 16:54:41 +0800

    新增chatgpt apple注册方法

---
+++ assets/index.html-39e0336d.js
@@ -0,0 +1,22 @@
+import{_ as e,o as t,c,a as n}from"./app-28f5d3d5.js";const r={};function s(a,i){return t(),c("div",null,[n(" ## 快速开始 "),n(` **npx**
+
+\`\`\`bash
+# 初始化，并选择 2.x
+npx @vuepress-reco/theme-cli init
+\`\`\`
+
+**npm**
+
+\`\`\`bash
+# 初始化，并选择 2.x
+npm install @vuepress-reco/theme-cli@1.0.7 -g
+theme-cli init
+\`\`\`
+
+**yarn**
+
+\`\`\`bash
+# 初始化，并选择 2.x
+yarn global add @vuepress-reco/theme-cli@1.0.7
+theme-cli init
+\`\`\` `)])}const o=e(r,[["render",s],["__file","index.html.vue"]]);export{o as default};
---
+++ assets/txYun.html-89298d70.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-5bc496f1","path":"/docs/theme-reco/localMarkdown/txYun.html","title":"","lang":"en-US","frontmatter":{},"headers":[],"git":{"createdTime":1687769175000,"updatedTime":1687769175000,"contributors":[{"name":"64986","email":"64986@yangzijiang.com","commits":1}]},"filePathRelative":"docs/theme-reco/localMarkdown/txYun.md"}');export{e as data};
